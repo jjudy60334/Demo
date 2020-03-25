afd1c6911221b3e22d2099ea240bb0b301ed3f18
Claire Wu <jjudy708618@gmail.com>
2020-03-26 01:19:57 +0800

    first upload

--- test_data_type.py
+++ test_data_type.py
@@ -4,11 +4,10 @@ from ast import literal_eval
 import time
 def data_type(data,column_dict):
     data_json=eval(data)
-    # df=pd.DataFrame.from_dict([data_json])
+    
     for c in column_dict:
         if column_dict[c] in [int,float]:
-            # print (data_json[c])
-            # print (c,isinstance(5, int))
+         
             try :
                column_dict[c](data_json[c])
                    
